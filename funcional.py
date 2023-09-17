60ce9a47972809ec639f6fd8405a5dcb29fd6a9e
cesar@cesar <sarcemandi@gmail.com>
2023-09-17 10:51:45 -0300

    funcional atualizado

--- funcional.py
+++ funcional.py
@@ -1,19 +1,33 @@
-from tratamento import Tratamento
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import RSLPStemmer
 
+def preprocess_text(text):
+    # Transforma cada letra em um token
+    tokenizacao = nltk.word_tokenize(text)
 
+    # Remove palavras comuns
+    lista_brasileiro = stopwords.words('portuguese')
+    remove_letras = [palavra for palavra in tokenizacao if palavra.lower() not in lista_brasileiro]
+
+    # Stemming
+    stemmer = RSLPStemmer()
+    stemizador = [stemmer.stem(palavra) for palavra in remove_letras]
+
+    # Combine todas as transformações em um único texto tratado
+    texto_tratado = ' '.join(stemizador)
+
+    return texto_tratado
 
 def main():
+    # Inseri texto
+    texto = input('Digite um texto: ')
 
-    texto = input('Digite: ')
+    # Pré-processa o texto
+    texto_tratado = preprocess_text(texto)
 
-    tratamento = Tratamento(texto)
-    tokenizacao = tratamento.tokeniza_por_letras()
-    mini = tratamento.minuscula()
-    remove_letras = tratamento.remover_palavras_comum()
-    stemizador = tratamento.stemizar()
-    dt = tratamento.criar_dataframe()
-    resultado = tokenizacao, mini, remove_letras, stemizador, dt
-    return resultado
+    # Imprimir o resultado final
+    print(f"Texto tratado: {texto_tratado}")
 
 if __name__ == "__main__":
     main()
--- tratamento.py
+++ tratamento.py
@@ -3,10 +3,9 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem import RSLPStemmer
 
-
 nltk.download('punkt')
 nltk.download('stopwords')
-nltk.download("rslp")
+nltk.download('rslp')
 
 class Tratamento:
 
@@ -16,31 +15,31 @@ class Tratamento:
     # Cria um dataframe
     def criar_dataframe(self):
         dt = pd.DataFrame({'coluna_de_dados': [self.dados]}, index=[0])
-        return dt
+        return dt  # Adicione um retorno para o DataFrame
 
     # Tokeniza por letras
     def tokeniza_por_letras(self):
         tokenizado = nltk.word_tokenize(self.dados)
-        return tokenizado
+        return tokenizado  # Adicione um retorno para a lista tokenizada
 
     # Tokeniza por frases
     def tokeniza_por_sentenca(self):
         token_sentenca = nltk.sent_tokenize(self.dados)
-        return token_sentenca
+        return token_sentenca  # Adicione um retorno para as sentenças tokenizadas
 
     # Deixa minusculo
     def minuscula(self):
         mini = str.lower(self.dados)
-        return mini
+        return mini  # Adicione um retorno para o texto em minúsculas
 
     # Retona somente palavras relevantes
     def remover_palavras_comum(self):
         lista_brasileira = stopwords.words('portuguese')
         texto_sem_stopwords = [palavra for palavra in nltk.word_tokenize(self.dados) if palavra.lower() not in lista_brasileira]
-        return texto_sem_stopwords
+        return texto_sem_stopwords  # Adicione um retorno para o texto sem stopwords
 
     # Executado após remover_palavra_comum(), stemming.
     def stemizar(self):
         stemmer = RSLPStemmer()
         stemizado = [stemmer.stem(palavra) for palavra in nltk.word_tokenize(self.dados, language='portuguese')]
-        return stemizado
+        return stemizado  # Adicione um retorno para as palavras stemizadas
